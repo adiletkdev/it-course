433d1464458770fc726dbc7d815961161fdc26f0
Adilet K <adiletk312@gmail.com>
2021-07-18 14:46:18 +0600

    Implement mobile responsive

--- next.config.js
+++ next.config.js
@@ -1,29 +1,7 @@
 module.exports = {
-  reactStrictMode: true,
-};
-// module.exports = {
-//   // Webpack 5 is enabled by default
-//   // You can still use webpack 4 while upgrading to the latest version of Next.js by adding the "webpack5: false" flag
-//   webpack5: true,
-// };
-// module.exports = {
-//   webpack(config) {
-//     config.module.rules.push({
-//       test: /\.svg$/,
-//       issuer: {
-//         test: /\.(js|ts)x?$/,
-//       },
-//       use: ['@svgr/webpack'],
-//     });
-
-//     return config;
-//   },
-// };
-
-module.exports = {
-  // other configs...
-
-  // future: { webpack5: true }, // -- not needed since Next.js v11.0.0
+  images: {
+    domains: ['courses-top.ru']
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
